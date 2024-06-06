eb59a7b1695736e927d919478e82bb3eca148bd5
cristiangligan <cristian.gligan@gmail.com>
2024-06-06 19:11:17 +0200

    SignInScreen new

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,9 +2,39 @@ package controller;
 
 import view.SignInScreen;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.io.File;
+
 public class Controller {
+    private SignInScreen signInScreen;
+    public Controller() {
+        signInScreen = new SignInScreen(this);
+    }
+
+    public void handleGoToSignIn() {
+
+    }
+
+    public void handleChooseProfilePic() {
+        SwingUtilities.invokeLater(() -> {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileFilter(new FileNameExtensionFilter("Images", "jpg", "jpeg", "png", "gif", "bmp"));
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            int option = fileChooser.showOpenDialog(null);
+            if (option == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+                String filePath = file.getAbsolutePath();
+                Image image = new ImageIcon(filePath).getImage();
+                Image newImage = image.getScaledInstance(SignInScreen.PROFILE_IMG_SIZE, SignInScreen.PROFILE_IMG_SIZE, Image.SCALE_SMOOTH);
+                Icon icon = new ImageIcon(newImage);
+                signInScreen.updateProfilePic(icon);
+            }
+        });
+    }
+
     public static void main(String[] args) {
         Controller controller = new Controller();
-        SignInScreen signInScreen = new SignInScreen(controller);
     }
 }
