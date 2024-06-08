94313be211df14d53c560a4759b4a9d54898bdb1
cristiangligan <cristian.gligan@gmail.com>
2024-06-08 12:40:38 +0200

    SignInScreen new

--- src/view/SignInScreen.java
+++ src/view/SignInScreen.java
@@ -9,20 +9,20 @@ import java.net.URL;
 import java.util.Map;
 
 public class SignInScreen extends JFrame {
-    private static final int FRAME_WIDTH = 460;
-    private static final int FRAME_HEIGHT = 220;
-    private static final int PROFILE_IMG_SIZE = 100;
+    private static final int FRAME_WIDTH = 280;
+    private static final int FRAME_HEIGHT = 320;
+    public static final int PROFILE_IMG_SIZE = 100;
+    private static final int DEFAULT_SPACING_PROFILE = 82;
     private static final int DEFAULT_SPACING = 20;
-    private static final int SIGN_UP_BTN_SPACE = 5;
+
 
 
     private final JPanel pnlMain = new JPanel();
-    private final JLabel lblProfilePic = new JLabel();
+    private final JButton btnChooseProfilePic = new JButton();
     private final JTextField txtUsername = new JTextField();
-    private final JTextField txtPassword = new JTextField();
     private final JButton btnSignIn = new JButton();
-    private final JButton btnGoToSignUp = new JButton();
-    private final JLabel lblAlreadyHaveAcc = new JLabel();
+    private final JButton btnGoToSignIn = new JButton();
+    private Icon profilePic;
     private final Controller controller;
 
     public SignInScreen(Controller controller) {
@@ -32,47 +32,31 @@ public class SignInScreen extends JFrame {
         setContentPane(pnlMain);
         pnlMain.setLayout(springLayout);
 
-        URL blankProfileImgUrl = ClassLoader.getSystemResource("images/blank-profile-picture.png");
+        URL blankProfileImgUrl = ClassLoader.getSystemResource("images/add-profile-picture.png");
         Image image = new ImageIcon(blankProfileImgUrl).getImage();
         Image newImage = image.getScaledInstance(PROFILE_IMG_SIZE, PROFILE_IMG_SIZE, Image.SCALE_SMOOTH);
-        Icon icon = new ImageIcon(newImage);
-        lblProfilePic.setIcon(icon);
-        lblProfilePic.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-        pnlMain.add(lblProfilePic);
-        springLayout.putConstraint(SpringLayout.NORTH, lblProfilePic, DEFAULT_SPACING, SpringLayout.NORTH, pnlMain);
-        springLayout.putConstraint(SpringLayout.WEST, lblProfilePic, DEFAULT_SPACING, SpringLayout.WEST, pnlMain);
+        profilePic = new ImageIcon(newImage);
+        btnChooseProfilePic.setIcon(profilePic);
+        btnChooseProfilePic.setBorder(BorderFactory.createTitledBorder("Click to set"));
+        btnChooseProfilePic.addActionListener(e -> onChooseProfileBtnClick());
+        pnlMain.add(btnChooseProfilePic);
+        springLayout.putConstraint(SpringLayout.NORTH, btnChooseProfilePic, DEFAULT_SPACING, SpringLayout.NORTH, pnlMain);
+        springLayout.putConstraint(SpringLayout.WEST, btnChooseProfilePic, DEFAULT_SPACING_PROFILE, SpringLayout.WEST, pnlMain);
+        springLayout.putConstraint(SpringLayout.EAST, btnChooseProfilePic, -DEFAULT_SPACING_PROFILE, SpringLayout.EAST, pnlMain);
+
 
         txtUsername.setBorder(BorderFactory.createTitledBorder("Username"));
         pnlMain.add(txtUsername);
-        springLayout.putConstraint(SpringLayout.NORTH, txtUsername, 0, SpringLayout.NORTH, lblProfilePic);
-        springLayout.putConstraint(SpringLayout.WEST, txtUsername, DEFAULT_SPACING, SpringLayout.EAST, lblProfilePic);
+        springLayout.putConstraint(SpringLayout.NORTH, txtUsername, DEFAULT_SPACING, SpringLayout.SOUTH, btnChooseProfilePic);
+        springLayout.putConstraint(SpringLayout.WEST, txtUsername, DEFAULT_SPACING, SpringLayout.WEST, pnlMain);
         springLayout.putConstraint(SpringLayout.EAST, txtUsername, -DEFAULT_SPACING, SpringLayout.EAST, pnlMain);
-
-        txtPassword.setBorder(BorderFactory.createTitledBorder("Password"));
-        pnlMain.add(txtPassword);
-        springLayout.putConstraint(SpringLayout.SOUTH, txtPassword, 0, SpringLayout.SOUTH, lblProfilePic);
-        springLayout.putConstraint(SpringLayout.WEST, txtPassword, 0, SpringLayout.WEST, txtUsername);
-        springLayout.putConstraint(SpringLayout.EAST, txtPassword, 0, SpringLayout.EAST, txtUsername);
 
         btnSignIn.setText("Sign in");
         pnlMain.add(btnSignIn);
-        springLayout.putConstraint(SpringLayout.NORTH, btnSignIn, DEFAULT_SPACING, SpringLayout.SOUTH, txtPassword);
-        springLayout.putConstraint(SpringLayout.EAST, btnSignIn, 0, SpringLayout.EAST, txtPassword);
-
-        lblAlreadyHaveAcc.setText("Already have an account?");
-        pnlMain.add(lblAlreadyHaveAcc);
-        springLayout.putConstraint(SpringLayout.NORTH, lblAlreadyHaveAcc, 0, SpringLayout.NORTH, btnSignIn);
-        springLayout.putConstraint(SpringLayout.WEST, lblAlreadyHaveAcc, 0, SpringLayout.WEST, lblProfilePic);
-        springLayout.putConstraint(SpringLayout.SOUTH, lblAlreadyHaveAcc, 0, SpringLayout.SOUTH, btnSignIn);
-
-        btnGoToSignUp.setText("Sign up");
-        pnlMain.add(btnGoToSignUp);
-        btnGoToSignUp.setBorder(BorderFactory.createEmptyBorder());
-        btnGoToSignUp.setRolloverEnabled(true);
-        btnGoToSignUp.getModel().addChangeListener(e -> onHoverSignUp());
-        springLayout.putConstraint(SpringLayout.NORTH, btnGoToSignUp, 0, SpringLayout.NORTH, lblAlreadyHaveAcc);
-        springLayout.putConstraint(SpringLayout.WEST, btnGoToSignUp, SIGN_UP_BTN_SPACE, SpringLayout.EAST, lblAlreadyHaveAcc);
-        springLayout.putConstraint(SpringLayout.SOUTH, btnGoToSignUp, 0, SpringLayout.SOUTH, lblAlreadyHaveAcc);
+        springLayout.putConstraint(SpringLayout.NORTH, btnSignIn, DEFAULT_SPACING, SpringLayout.SOUTH, txtUsername);
+        springLayout.putConstraint(SpringLayout.WEST, btnSignIn, DEFAULT_SPACING, SpringLayout.WEST, txtUsername);
+        springLayout.putConstraint(SpringLayout.EAST, btnSignIn, -DEFAULT_SPACING, SpringLayout.EAST, txtUsername);
+        springLayout.putConstraint(SpringLayout.SOUTH, btnSignIn, -DEFAULT_SPACING, SpringLayout.SOUTH, pnlMain);
 
         this.pack();
         this.setVisible(true);
@@ -82,18 +66,27 @@ public class SignInScreen extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    private void onChooseProfileBtnClick() {
+        controller.handleChooseProfilePic();
+    }
+
     private void onHoverSignUp() {
-        Font btnFont = btnGoToSignUp.getFont();
+        Font btnFont = btnGoToSignIn.getFont();
         Map attributes = btnFont.getAttributes();
-        if (btnGoToSignUp.getModel().isRollover()) {
+        if (btnGoToSignIn.getModel().isRollover()) {
 
             attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
-            btnGoToSignUp.setForeground(Color.BLUE);
+            btnGoToSignIn.setForeground(Color.BLUE);
         } else {
             attributes.put(TextAttribute.UNDERLINE, null);
-            btnGoToSignUp.setForeground(Color.BLACK);
+            btnGoToSignIn.setForeground(Color.BLACK);
         }
         btnFont = btnFont.deriveFont(attributes);
-        btnGoToSignUp.setFont(btnFont);
+        btnGoToSignIn.setFont(btnFont);
+    }
+
+    public void updateProfilePic(Icon profilePic) {
+        this.profilePic = profilePic;
+        btnChooseProfilePic.setIcon(profilePic);
     }
 }
